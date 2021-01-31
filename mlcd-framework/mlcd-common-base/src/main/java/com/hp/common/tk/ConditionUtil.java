82c885bbeb8229a935194c4c088e21c1d786fde0
mcfan <fan957605155@gmail.com>
2021-01-31 20:11:44 +0800

    test

---
+++ mlcd-framework/mlcd-common-base/src/main/java/com/hp/common/tk/ConditionUtil.java
@@ -0,0 +1,56 @@
+package com.hp.common.tk;
+
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+
+
+/**
+ * tk条件工具封装
+ */
+
+public class ConditionUtil {
+
+    private ConditionUtil(){}
+
+
+    public static Condition buildCondition(Class<?> clazz, List<?> list){
+        return new Condition() {
+            @Override
+            public void await() throws InterruptedException {
+
+            }
+
+            @Override
+            public void awaitUninterruptibly() {
+
+            }
+
+            @Override
+            public long awaitNanos(long nanosTimeout) throws InterruptedException {
+                return 0;
+            }
+
+            @Override
+            public boolean await(long time, TimeUnit unit) throws InterruptedException {
+                return false;
+            }
+
+            @Override
+            public boolean awaitUntil(Date deadline) throws InterruptedException {
+                return false;
+            }
+
+            @Override
+            public void signal() {
+
+            }
+
+            @Override
+            public void signalAll() {
+
+            }
+        };
+    }
+}
