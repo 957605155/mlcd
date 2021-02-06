272f260de7c8b299c1541eff5329db49d7768f6d
957605155 <fan957605155@gmail.com>
2021-02-06 19:54:56 +0800

    AuditParam
    AuditParam

--- mlcd-framework/mlcd-common-base/src/main/java/com/hp/common/base/AuditParam.java
+++ mlcd-framework/mlcd-common-base/src/main/java/com/hp/common/base/AuditParam.java
@@ -1,8 +1,13 @@
 package com.hp.common.base;
 
 public class AuditParam {
-             public void test(){
-               Sysytem.out.printf("1234");
-             }
+    @ApiModelProperty(value="审核的记录编号集合",required=true)
+    @NotEmpty(message="审核的记录编号集合不能为空")
+    private List<Long> ids;
+    @ApiModelProperty(value="审核不通过原因")
+    private String rejectReason;
+    @ApiModelProperty(value="是否通过(1->否,2->是)",required=true)
+    @NotNull(message="通过标识不能为空")
+    private YesNoEnum pass;
 
 }
