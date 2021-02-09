c2aad450a78f66ca50af8affa16a974961848726
mcfan <fan957605155@gmail.com>
2021-02-09 22:03:53 +0800

    CodedEnum类

---
+++ mlcd-framework/mlcd-common-base/src/main/java/com/hp/common/base/CodedEnum.java
@@ -0,0 +1,13 @@
+package com.hp.common.base;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public interface CodedEnum {
+    int getValue();
+    String getName();
+
+    public static <E extends Enum<?> & CodedEnum> Optional<E> codeOf(Class<E> enumClass,int code){
+        return Arrays.stream(enumClass.getEnumConstants()).filter(e -> e.getValue() == code).findAny();
+    }
+}
